407b08c9e1e689086ebf842cc40ddf03c81d7051
Ianluck Rop <ianluckrp@ciencias.unam.mx>
2025-09-26 19:29:36 -0600

    Defimimos una version preliminar de la Gramatica

--- proyecto1/src/ASA.hs
+++ proyecto1/src/ASA.hs
@@ -25,3 +25,124 @@ data Expr
   | ELambda Var Expr
   | EApp Expr Expr
   deriving (Show, Eq)  
+
+
+-- Correccion echa por manolito
+
+-- Tokens para el Parser
+data Token
+  -- Literales
+  = TokenVar String
+  | TokenNum Int
+  | TokenBool Bool
+
+  -- Paréntesis
+  | TokenPA       -- (
+  | TokenPC       -- )
+
+  -- Corchetes y listas
+  | TokenLI       -- [
+  | TokenLD       -- ]
+  | TokenComma    -- ,
+
+  -- Operadores aritméticos
+  | TokenAdd      -- +
+  | TokenSub      -- -
+  | TokenMul      -- *
+  | TokenDiv      -- /
+  | TokenAdd1     -- add1
+  | TokenSub1     -- sub1
+  | TokenSqrt     -- sqrt
+  | TokenExpt     -- expt
+
+  -- Operadores booleanos / comparaciones
+  | TokenNot      -- not
+  | TokenEq       -- =
+  | TokenNeq      -- !=
+  | TokenLt       -- <
+  | TokenGt       -- >
+  | TokenLeq      -- <=
+  | TokenGeq      -- >=
+
+  -- Palabras clave de control
+  | TokenLet      -- let
+  | TokenLetRec   -- letrec
+  | TokenLetStar  -- let*
+  | TokenIf0      -- if0
+  | TokenIf       -- if
+  | TokenCond     -- cond
+  | TokenElse     -- else
+
+  -- Funciones y aplicación
+  | TokenLambda   -- lambda
+  | TokenApp      -- app (opcional si se quiere sintaxis explícita)
+
+  -- Pares y listas
+  | TokenPair     -- pair
+  | TokenFst      -- fst
+  | TokenSnd      -- snd
+  | TokenHead     -- head
+  | TokenTail     -- tail
+
+  deriving (Show, Eq)
+
+
+--- ASA con azucar (ASA)
+data ASA
+  = Var String              -- variable
+  | Num Int                 -- enteros
+  | Bool Bool               -- booleanos
+  | Add [ASA]               -- suma variádica
+  | Sub [ASA]               -- resta variádica
+  | Mul [ASA]               -- multiplicación variádica
+  | Div [ASA]               -- división variádica
+  | Not ASA                 -- negación
+  | Let [(String, ASA)] ASA -- let paralelo
+  | LetRec [(String, ASA)] ASA -- letrec
+  | LetStar [(String, ASA)] ASA -- let* secuencial
+  | If0 ASA ASA ASA         -- if0
+  | If ASA ASA ASA          -- if booleano
+  | Lambda [ASA] ASA     -- funciones anónimas (variádicas)
+  | App ASA [ASA]           -- aplicación: (f x y z)
+  | Add1 ASA                -- incremento
+  | Sub1 ASA                -- decremento
+  | Sqrt ASA                -- raíz cuadrada
+  | Expt ASA ASA            -- potencia
+  | Equal [ASA]             -- igualdad variádica
+  | Greater [ASA]           -- mayor que (variádica)
+  | Less [ASA]              -- menor que (variádica)
+  | GEqual [ASA]            -- >=
+  | LEqual [ASA]            -- <=
+  | Diff [ASA]              -- !=
+  | Pair ASA ASA            -- pares ordenados
+  | Fst ASA                 -- proyección 1
+  | Snd ASA                 -- proyección 2
+  | List [ASA]              -- listas [1,2,3]
+  | Head ASA                -- cabeza
+  | Tail ASA                -- cola
+  | Cond [(ASA, ASA)]       -- cond [(cond, expr), ..., (else, expr)]
+  deriving (Show, Eq)
+
+
+--Desazucaramos
+
+-- ASA sin azucar (AST)
+data AST
+  = CVar String
+  | CNum Int
+  | CBool Bool
+  | CAdd AST AST
+  | CSub AST AST
+  | CMul AST AST
+  | CDiv AST AST
+  | CIf AST AST AST
+  | CLambda String AST
+  | CAPP AST AST
+  | CPair AST AST
+  | CFst AST
+  | CSnd AST
+  | CList [AST]
+  | CHead AST
+  | CTail AST
+  | CCond [(AST, AST)]
+  deriving (Show, Eq)
